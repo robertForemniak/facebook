d22d46ed4439c313c94fb4bf41c6677732c736c9
robert foremniak <robert.fff82@gmail.com>
2019-06-25 08:56:09 +0200

    drugi dzien

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,8 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { CoreRoutingModule } from './core-routing.module';
-import { AppComponent } from './components/app/app.component';
-import { MenuComponent } from './components/menu/menu.component';
+import { AppComponent } from '../core/components/app/app.component';
+import { MenuComponent } from '../core/components/menu/menu.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
 import { PostsModule } from '../posts/posts.module';
